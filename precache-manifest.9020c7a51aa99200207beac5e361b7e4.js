1b635eed2884fb0b735eda42b0a664511c608e6c
dat295 <jvdat15@gmail.com>
2020-12-04 00:07:31 +0700

    Updates

---
+++ precache-manifest.9020c7a51aa99200207beac5e361b7e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdb28b436d91e98eec6adf7c141116f",
+    "url": "/Movie_Final_Project/index.html"
+  },
+  {
+    "revision": "abe11ebbd36b1248895b",
+    "url": "/Movie_Final_Project/static/css/main.82f6daed.chunk.css"
+  },
+  {
+    "revision": "ea6bd5b8c0cae7e08153",
+    "url": "/Movie_Final_Project/static/js/2.9a7e9699.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/Movie_Final_Project/static/js/2.9a7e9699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe11ebbd36b1248895b",
+    "url": "/Movie_Final_Project/static/js/main.1db7c6c9.chunk.js"
+  },
+  {
+    "revision": "e0d8147d95f32e9b36e6",
+    "url": "/Movie_Final_Project/static/js/runtime-main.3f32e901.js"
+  }
+]);
